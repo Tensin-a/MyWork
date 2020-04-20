d4d66669ea2c7e3c068edb6bf175d61878bc2a67
Alex <koki.pap@yandex.ru>
2020-04-20 18:04:50 +0400

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/*console.log(4/0);
+console.log('string'*9);
+
+let persone = {
+    name: "Jone",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.age);
+*/
+let arr = ['plum.png','orange.jpg','apple.bmp']
+/*console.log(arr[2]);
+
+console.log (typeof(String(4))); // 1. первый малоиспользуемый метод динамической типизации.
+console.log(typeof ("ww " + 5));
+console.log('https//vk.com/catalog/' + 5); // 2. преврашение даннх в строку (string)
+
+console.log (typeof(Number('5'))); // 1
+
+console.log(typeof (5 + +'5')) // 2. + перед значением называется унарным и превращает тип данных в число
+
+console.log (typeof(Number('5')));
+
+0, '', null, undefined, NaN
+
+let switcher = null;
+
+if  (switcher) {
+    console.log("Working..")
+}
+*/
+//alert("Hello world");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+/*
+let answer = +prompt("есть ли вам 18", "Да");
+
+console.log(typeof(answer));
+
+console.log('arr' + '- object');
+console.log(4 + '- object');
+*/
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
